530b0fb1ddd7c20c46bb2372ca112eb5f880ddab
JGM <cnxw4570123@gmail.com>
2023-01-19 17:34:36 +0900

    day06 review manual decorator with closure

--- day06_solo.py
+++ day06_solo.py
@@ -1,11 +1,20 @@
-#
-def return1():
-    return 1
+# manual decorator
+
+def func_info(f):
+    def info(*args, **kwargs):
+        print('func name : ', f.__name__)
+        print('positional param: ', args)
+        print('keyword param:', kwargs)
+        result = f(*args, **kwargs)
+        print('result : ', result)
+        return result
+    return info
 
 
-def print1():
-    print(1)
+def list_into_dict(list1, list2):
+    dict1 = {w1 : w2 for w1, w2 in zip(list1, list2)}
+    return dict1
 
-print(type(return1()))
-print()
-print(type(print1()))
+
+res1 = func_info(list_into_dict)
+res1(['a', 'b', 'c', 'd'],[1, 2, 3, 4])
