1fea554e79f379cc7178e7d60f950dae68bc03e5
JGM <cnxw4570123@gmail.com>
2023-01-30 18:18:06 +0900

    Stack with tkinter

--- stack.py
+++ stack.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -36,10 +39,10 @@ def infix_to_postfix(infix):
     prec = {
         '(': 0,
         '+': 1,
-        '-': 2,
-        '*': 3,
-        '/': 4,
-        '^': 5       
+        '-': 1,
+        '*': 2,
+        '/': 2,
+        '^': 3       
     }
     
     for token in token_list:
@@ -50,14 +53,57 @@ def infix_to_postfix(infix):
                 outstack.append(opstack.pop())
             opstack.pop()  # ( 제거
         elif token in '+-/*^':
-            while not opstack.isEmpty() and prec[opstack.top()] > prec[token]:
+            while not opstack.isEmpty() and prec[opstack.top()] >= prec[token]:
                 outstack.append(opstack.pop())
             opstack.push(token)
         else:
             outstack.append(token)
 
-    while not opstack.isEmpty():
+    while opstack:
         outstack.append(opstack.pop())
-    return " ".join(outstack)
+    return compute_postfix(" ".join(outstack))
 
-print(infix_to_postfix('( a + b ) * c'))
+
+def compute_postfix(postfix):
+    result = Stack()
+    token_list = str(postfix).split()
+    
+    for token in token_list:
+        if token in '+-*/^':
+            oprnd1 = float(result.pop())
+            oprnd2 = float(result.pop())
+            if token == '+':
+                result.push(oprnd2 + oprnd1)
+            elif token == '-':
+                result.push(oprnd2 - oprnd1)
+            elif token == '*':
+                result.push(oprnd2 * oprnd1)
+            elif token == '/':
+                result.push(oprnd2 / oprnd1)
+            elif token == '^':
+                result.push(oprnd2**oprnd1)
+        else:
+            result.push(float(token))
+    
+    return result.pop()
+
+
+def do_something():
+    value = compute_postfix(infix_to_postfix(expr.get()))
+    total.set("{0:.4f}".format(value))
+    return
+
+
+root = tk.Tk()
+root.title("My Calculator")
+expr = tk.StringVar()
+title_label = tk.Label(root, text="My Calcualtor").grid(row=0, columnspan=2)
+input_exam = tk.Label(root, text="Space between terms: ( 3 + 2 ) * 8").grid(row=1, columnspan=2)
+exp_entry = tk.Entry(root, textvariable=expr).grid(row=2, column=0)
+total_label = tk.Label(root, text="TOTAL").grid(row=3, column=0)
+total = tk.StringVar()
+total.set('0')
+value_label = tk.Label(root, textvariable=total, width=20).grid(row=3, column=1)
+equal_btn = tk.Button(root, text=' = ', width=20, command=do_something).grid(row=2, column=1)
+root.mainloop()
+root.destroy()
