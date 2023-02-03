6be8bd32c745a813fa3aa5bdbd8424b7b7fd3b7a
JGM <cnxw4570123@gmail.com>
2023-02-03 11:19:36 +0900

    23/02/03 black

--- day05_review.py
+++ day05_review.py
@@ -9,29 +9,29 @@
 # multiply(lambda num1, num2 : (num1 * num2), num1_list, num2_list)
 
 drinks = {
-    'martini': {'vodka', 'vermouth'},
-    'black russian': {'vodka', 'kahlua'},
-    'white russian': {'cream', 'kahlua', 'vodka'},
-    'manhattan': {'rye', 'vermouth', 'bitters'},
-    'screwdriver': {'orange juice', 'vodka'}
+    "martini": {"vodka", "vermouth"},
+    "black russian": {"vodka", "kahlua"},
+    "white russian": {"cream", "kahlua", "vodka"},
+    "manhattan": {"rye", "vermouth", "bitters"},
+    "screwdriver": {"orange juice", "vodka"},
 }
 
 for key, contents in drinks.items():
     # if 'vodka' in contents and not ('vermouth' in contents or 'cream' in contents):
     #     print(key)
-    if contents & {'vermouth', 'orange juice'}:
+    if contents & {"vermouth", "orange juice"}:
         print(key)
 
-bruss = drinks['black russian']
-wruss = drinks['white russian']
+bruss = drinks["black russian"]
+wruss = drinks["white russian"]
 
 a = {1, 2}
 b = {2, 3}
 
-print(a&b)
+print(a & b)
 print(a.intersection(b))
-print(bruss&wruss)
-print(a|b)
+print(bruss & wruss)
+print(a | b)
 print(a.union(b))
 print(bruss.union(wruss))
 print(bruss.symmetric_difference(wruss))
