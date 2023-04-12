ab56fe6e404c0741d0420beaa9a1d70996b14b16
JGM <cnxw4570123@gmail.com>
2023-04-13 01:19:22 +0900

    1167

--- prob2.py
+++ prob2.py
@@ -1,35 +1,36 @@
 from sys import stdin
-from collections import deque
-
-n = int(stdin.readline().strip())
-tree_list = [[] for _ in range(n + 1)]
-count = n - 1
-while count > 0:
-    count -= 1
-    node1, node2 = list(map(int, stdin.readline().split()))
-    tree_list[node1].append(node2)
-    tree_list[node2].append(node1)
 
 
-visited = [False] * (n + 1)
-
-
-def BFS(graph, visited, idx):
-    prnt_list = [0 for _ in range(n + 1)]
-    queue = deque()
-    queue.append(idx)
+def DFS(graph, idx):
+    visited = [False for _ in range(n + 1)]
+    cost = [0 for _ in range(n + 1)]
+    stack = [idx]
     visited[idx] = True
-    while queue:
-        current = queue.popleft()
+    while stack:
+        current = stack.pop()
         if not visited[current]:
             visited[current] = True
-        for child in graph[current]:
+        for child, len in graph[current]:
             if not visited[child]:
-                prnt_list[child] = current
-                queue.append(child)
-    return prnt_list
+                stack.append(child)
+                cost[child] += cost[current] + len
+    max_val = max(cost)
+    index = cost.index(max_val)
+    return (index, max_val)
 
 
-prnt_list = BFS(tree_list, visited, 1)
-for val in prnt_list[2:]:
-    print(val)
+n = int(stdin.readline().strip())
+graph = [[] for _ in range(n + 1)]
+count = n
+while count > 0:
+    count -= 1
+    v_list = list(map(int, stdin.readline().split()))
+    for idx in range(len(v_list) // 2 - 1):
+        v = (idx * 2) + 1
+        v_len = (idx + 1) * 2
+        graph[v_list[0]].append([v_list[v], v_list[v_len]])
+
+node, cost = DFS(graph, 1)
+node2, cost2 = DFS(graph, node)
+
+print(max(cost, cost2))
