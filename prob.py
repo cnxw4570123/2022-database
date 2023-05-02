61417f15dae32c0575c59828327f9062a785555c
JGM <cnxw4570123@gmail.com>
2023-05-02 14:29:30 +0900

    1260

--- prob.py
+++ prob.py
@@ -1,32 +1,46 @@
-# prob : 1018
-# https://www.acmicpc.net/problem/1018
+# prob : 1260
+# https://www.acmicpc.net/problem/1260
 
-import sys
+from sys import stdin
+from collections import deque
 
-input = sys.stdin.readline
-n, m = map(int, input().split())  # 세로, 가로
-checker = [input().rstrip() for _ in range(n)]
-color = ["W", "B"]
-cnt = 0
-ans = []
-i = j = 0
+input = stdin.readline
 
 
-while i + 7 < n and j + 7 < m:
-    left = checker[i][j]
-    idx = color.index(left)
-    for h in range(i, i + 8):
-        for w in range(j, j + 8):  # 가로 한 칸씩 변경
-            if color[idx] != checker[h][w]:  # 다시 색칠해야 하면
-                cnt += 1
-            idx = (idx + 1) % 2  # color안에서 계속 바뀌도록 함
-        idx = (idx + 1) % 2  # h 변경, 변을 공유하므로 색 변경
+def BFS(graph, idx):
+    visited = [False] * (n + 1)
+    q = deque()
+    q.append(idx)
+    while q:
+        current = q.popleft()
+        if not visited[current]:
+            visited[current] = True
+            print(current, end=" ")
+        for c in range(len(graph[current])):
+            if graph[current][c] and not visited[c]:
+                q.append(c)
 
-    ans.append(min(cnt, 64 - cnt))
-    cnt = 0
-    j += 1
-    if j + 7 == m:  # 가로의 모든 칸을 탐색하면
-        i += 1  # 세로 한 칸 이동
-        j = 0  # 가로 초기화
 
-print(min(ans))
+def DFS(graph, idx):
+    visited = [False] * (n + 1)
+    stack = [idx]
+    while stack:
+        current = stack.pop()
+        if not visited[current]:
+            visited[current] = True
+            print(current, end=" ")
+        for c in range(len(graph[current]) - 1, -1, -1):
+            if graph[current][c] and not visited[c]:
+                stack.append(c)
+
+
+n, m, v = map(int, input().split())
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+DFS(graph, v)
+print()
+BFS(graph, v)
