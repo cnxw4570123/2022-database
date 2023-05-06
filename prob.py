65357c860def828795e5445441046099896de913
JGM <cnxw4570123@gmail.com>
2023-05-07 04:38:22 +0900

    3078

--- prob.py
+++ prob.py
@@ -1,44 +1,32 @@
-# prob : 1303
-# https://www.acmicpc.net/problem/1303
+# prob : 3078
+# https://www.acmicpc.net/problem/3078
 
 import sys
-
+from collections import deque
 
 input = sys.stdin.readline
-sys.setrecursionlimit(100000)
 
 
-def distinguish(h, w, graph, v, color):
-    cnt = 0
-    for y, x in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-        ny, nx = h + y, w + x
-        if 0 <= ny <= m - 1 and 0 <= nx <= n - 1:
-            if graph[ny][nx] == color and not v[ny][nx]:
-                v[ny][nx] = True
-                cnt = cnt + 1 + distinguish(ny, nx, graph, v, color)
-    return cnt
+n, k = map(int, input().split())
+friends = [len(input().rstrip()) for _ in range(n)]
+ans = 0
 
+length = [0 for _ in range(21)]
 
-w_power, b_power = 0, 0
+# length와 deque에 k개의 원소 넣어서 준비
+q = deque()
+for elm in friends[0 : k + 1]:
+    length[elm] += 1
+    q.append(elm)
 
-n, m = map(int, input().split())  # 가로, 세로
+idx = k + 1
 
-war_zone = [input().rstrip() for _ in range(m)]
-
-v = [[False] * n for _ in range(m)]
-
-for h in range(m):
-    for w in range(n):
-        if war_zone[h][w] == "W":
-            if not v[h][w]:
-                v[h][w] = True
-                w_cnt = 1
-                w_cnt += distinguish(h, w, war_zone, v, "W")
-                w_power += w_cnt * w_cnt
-        else:
-            if not v[h][w]:
-                v[h][w] = True
-                b_cnt = 1
-                b_cnt += distinguish(h, w, war_zone, v, "B")
-                b_power += b_cnt * b_cnt
-print(w_power, b_power)
+while q:
+    current = q.popleft()
+    length[current] -= 1  # 자기 자신 제거
+    ans += length[current]
+    if idx < n:
+        q.append(friends[idx])
+        length[friends[idx]] += 1
+        idx += 1
+print(ans)
