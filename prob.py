4b7111438db2bdf9696a929de702c25db903805b
JGM <cnxw4570123@gmail.com>
2023-05-07 04:38:53 +0900

    14502

--- prob.py
+++ prob.py
@@ -1,32 +1,51 @@
-# prob : 3078
-# https://www.acmicpc.net/problem/3078
+# prob : 14502
+# https://www.acmicpc.net/problem/14502
 
 import sys
-from collections import deque
+import copy
 
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
 
-n, k = map(int, input().split())
-friends = [len(input().rstrip()) for _ in range(n)]
+def infected_route(h, w, graph, v):
+    for y, x in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+        ny = h + y
+        nx = w + x
+        if 0 <= ny <= n - 1 and 0 <= nx <= m - 1:
+            if not v[ny][nx] and not graph[ny][nx]:
+                v[ny][nx] = True
+                graph[ny][nx] = 2
+                infected_route(ny, nx, graph, v)
+
+
+n, m = map(int, input().split())  # 세로, 가로
 ans = 0
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-length = [0 for _ in range(21)]
-
-# length와 deque에 k개의 원소 넣어서 준비
-q = deque()
-for elm in friends[0 : k + 1]:
-    length[elm] += 1
-    q.append(elm)
-
-idx = k + 1
-
-while q:
-    current = q.popleft()
-    length[current] -= 1  # 자기 자신 제거
-    ans += length[current]
-    if idx < n:
-        q.append(friends[idx])
-        length[friends[idx]] += 1
-        idx += 1
+for i in range(n * m - 2):
+    if graph[i // m][i % m]:
+        continue
+    for j in range(i + 1, n * m - 1):
+        if graph[j // m][j % m]:
+            continue
+        for k in range(j + 1, n * m):
+            if graph[k // m][k % m]:
+                continue
+            graph_copy = copy.deepcopy(graph)
+            (
+                graph_copy[i // m][i % m],
+                graph_copy[j // m][j % m],
+                graph_copy[k // m][k % m],
+            ) = (1, 1, 1)
+            v = [[False] * m for _ in range(n)]
+            for h in range(n):
+                for w in range(m):
+                    if graph_copy[h][w] == 2 and not v[h][w]:
+                        infected_route(h, w, graph_copy, v)
+            safe_zone = 0
+            for line in graph_copy:
+                safe_zone += line.count(0)
+            ans = max(ans, safe_zone)
+            # print(f"i, j, k = {i, j, k}safe_zone = {safe_zone}")
 print(ans)
