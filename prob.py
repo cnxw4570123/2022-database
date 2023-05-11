3ebbae190d7f72766cb656f5002aa20ef2cd3da9
JGM <cnxw4570123@gmail.com>
2023-05-11 16:57:35 +0900

    1753

--- prob.py
+++ prob.py
@@ -1,42 +1,38 @@
-# prob : 7576
-# https://www.acmicpc.net/problem/7576
+# prob : 1753
+# https://www.acmicpc.net/problem/1753
 
 import sys
-from collections import deque
+import heapq
 
 input = sys.stdin.readline
 
+vertex, edge = map(int, input().split())
+graph = [[] for _ in range(vertex + 1)]
+k = int(input())
+for _ in range(edge):
+    u, v, w = map(int, input().split())
+    graph[u].append((w, v))  # 거리와 정점
 
-def BFS(graph, start):  # 탐색이 불가능하면 종료 Queue 생명주기랑 같음
-    direction = [[-1, 1, 0, 0], [0, 0, -1, 1]]
-    q = deque(start)
+
+def dijkstra(start):
+    costs = [float("INF")] * (vertex + 1)
+    costs[start] = 0
+    q = []
+    heapq.heappush(q, (0, start))  # 간선 가중치와 시작 위치
     while q:
-        current_y, current_x = q.popleft()
-        for i in range(4):
-            ny = direction[0][i] + current_y
-            nx = direction[1][i] + current_x
-            if 0 <= ny <= m - 1 and 0 <= nx <= n - 1:
-                if not graph[ny][nx]:
-                    q.append((ny, nx))
-                    graph[ny][nx] = graph[current_y][current_x] + 1
+        current_dist, current_pos = heapq.heappop(q)
+        for neighbor_dist, neigbor in graph[current_pos]:
+            new_dist = neighbor_dist + current_dist
+            if new_dist < costs[neigbor]:
+                costs[neigbor] = new_dist
+                heapq.heappush(q, (new_dist, neigbor))
+    return costs
 
 
-n, m = map(int, input().split())  # 가로, 세로
+costs = dijkstra(k)  # costs 반환
 
-tomatos = [list(map(int, input().split())) for _ in range(m)]
-days = 0
-path = []
-for h in range(m):
-    for w in range(n):
-        if tomatos[h][w] == 1:
-            path += [(h, w)]  # 출발점,날짜
-
-BFS(tomatos, path)
-
-# BFS 탐색 후 그래프 탐색하면서 0이 있으면 -1
-for line in tomatos:
-    if 0 in line:
-        days = -1
-        break
-    days = max(days, max(line) - 1)
-print(days)
+for i in range(1, vertex + 1):
+    if costs[i] == float("inf"):
+        print("INF")
+    else:
+        print(costs[i])
