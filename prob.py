527b76305e66498df7a33f7bb885b46d46c6d77d
JGM <cnxw4570123@gmail.com>
2023-05-26 15:53:09 +0900

    13023

--- prob.py
+++ prob.py
@@ -1,38 +1,39 @@
-# prob : 1753
-# https://www.acmicpc.net/problem/1753
+# prob : 13023
+# https://www.acmicpc.net/problem/13023
 
 import sys
-import heapq
 
 input = sys.stdin.readline
 
-vertex, edge = map(int, input().split())
-graph = [[] for _ in range(vertex + 1)]
-k = int(input())
-for _ in range(edge):
-    u, v, w = map(int, input().split())
-    graph[u].append((w, v))  # 거리와 정점
+
+def DFS(graph, start):
+    s = [(start, 0, set())]
+    while s:
+        current, cnt, v = s.pop()
+        if cnt == 4:
+            return 1
+        if current in v:
+            continue
+        v.add(current)
+        for child in graph[current]:
+            if child not in v:
+                s.append((child, cnt + 1, set(v)))
+    return 0
 
 
-def dijkstra(start):
-    costs = [float("INF")] * (vertex + 1)
-    costs[start] = 0
-    q = []
-    heapq.heappush(q, (0, start))  # 간선 가중치와 시작 위치
-    while q:
-        current_dist, current_pos = heapq.heappop(q)
-        for neighbor_dist, neigbor in graph[current_pos]:
-            new_dist = neighbor_dist + current_dist
-            if new_dist < costs[neigbor]:
-                costs[neigbor] = new_dist
-                heapq.heappush(q, (new_dist, neigbor))
-    return costs
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+ans = []
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
 
-costs = dijkstra(k)  # costs 반환
-
-for i in range(1, vertex + 1):
-    if costs[i] == float("inf"):
-        print("INF")
-    else:
-        print(costs[i])
+for i in range(n):
+    if DFS(graph, i) == 1:
+        print(1)
+        break
+else:
+    print(0)
